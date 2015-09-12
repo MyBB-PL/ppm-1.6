a53f0aec041711d54ab7d2e6de6aee53b5de83cc
Gigik <gigi@mybboard.pl>
2015-09-12 06:30:27 +0200

    Aktualizacja numerów plików tłumaczenia (1.6.18)

--- Upload/inc/languages/polish.php
+++ Upload/inc/languages/polish.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * MyBB 1.6.17 Polish Language Pack
- * Copyright © 2008-2014 MyBBoard.pl Team
+ * MyBB 1.6.18 Polish Language Pack
+ * Copyright © 2008-2015 MyBBoard.pl Team
  * See readme.html for copyright information. | Zapoznaj sie z plikiem czytaj_to.html.
  * Autorzy: bryn, DamYan, gigi, matc, szulcu | Testerzy: hippie
  */
@@ -16,7 +16,7 @@ $langinfo['author'] = "Polski Support MyBB";
 $langinfo['website'] = "http://www.mybboard.pl/";
 
 // Compatible version of MyBB
-$langinfo['version'] = "1617";
+$langinfo['version'] = "1618";
 
 // Sets if the translation includes the Admin CP (1 = yes, 0 = no)
 $langinfo['admin'] = 1;
